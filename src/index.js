656891e7ae0e59016dab16507f85bc94167fa06a
thneves <tsneves11@gmail.com>
2021-07-09 19:06:01 -0300

    Working Redux Store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import store from './store';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed!", store.getState());
+// })
+
+// unsubscribe();
+
+store.dispatch(bugAdded("Bug 1"));
+
+console.log(store.getState());
+
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
+
+store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
